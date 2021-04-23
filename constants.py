79fe1b8a0ae06932c1003b5311085b8beb5e8ed1
txoof <aaron.ciuffo@gmail.com>
2021-04-23 12:30:15 +0200

    fix error

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
-"monday" = 0
-"tuesday" = 1
-"wednesday" = 2
-"thursday" = 3
-"friday" = 4
-"saturday" = 5
-"sunday" = 7
+monday = 0
+tuesday = 1
+wednesday = 2
+thursday = 3
+friday = 4
+saturday = 5
+sunday = 7
